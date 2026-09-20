c57ebe7c670eb62c41905f2a721a9c49474d6f62
heyongqi10 <heyongqi10@jd.com>
2026-09-20 12:39:43 +0800

    Add RSS and Sitemap plugins to rspress.config.ts and update package.json and pnpm-lock.yaml

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,11 +1,14 @@
 import * as path from 'node:path';
 import { defineConfig } from '@rspress/core';
+import { pluginSitemap } from '@rspress/plugin-sitemap';
+import { pluginRss } from '@rspress/plugin-rss';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   lang: 'en',
   title: 'My Site',
   icon: '/rspress-icon.png',
+  llms: true,
   logo: {
     light: '/rspress-light-logo.png',
     dark: '/rspress-dark-logo.png',
@@ -19,4 +22,9 @@ export default defineConfig({
       },
     ],
   },
+  plugins: [pluginSitemap({
+    siteUrl: 'https://heyq02.github.io'
+  }), pluginRss({
+    siteUrl: 'https://heyq02.github.io'
+  })],
 });
